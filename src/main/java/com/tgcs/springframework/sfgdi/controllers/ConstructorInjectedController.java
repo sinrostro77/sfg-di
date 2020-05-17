8c2d60883228c754a02b833718ca15ef73915906
Julio Moreno <julio.moreno@toshibagcs.com>
2020-05-17 12:53:53 -0500

    Dependency Injection Type Exercises

---
+++ src/main/java/com/tgcs/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,17 @@
+package com.tgcs.springframework.sfgdi.controllers;
+
+import com.tgcs.springframework.sfgdi.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/test/java/com/tgcs/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package com.tgcs.springframework.sfgdi.controllers;
+
+import com.tgcs.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+    
+    SetterInjectedController controller;
+    
+
+    @BeforeEach
+    void setUp() {
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
